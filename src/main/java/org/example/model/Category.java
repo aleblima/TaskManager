2105139138d2ecdf4785ba8689951143b7074a83
Alejandro <alejandrolima1030@gmail.com>
2026-05-26 01:09:35 -0300

    model: update Category model to include id and remove old constructor

--- src/main/java/org/example/model/Category.java
+++ src/main/java/org/example/model/Category.java
@@ -2,9 +2,19 @@ package org.example.model;
 
 public class Category {
     private String category;
+    private int id;
 
-    public Category(String category){
+    public Category(String category, int id){
         this.category = category;
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
     }
 
     public String getCategory(){
