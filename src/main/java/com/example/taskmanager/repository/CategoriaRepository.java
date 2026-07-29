b8e7654ccf9a7fb251b6911eb436e67aaebb4213
Alejandro Lima <alejandrolima1030@gmail.com>
2026-07-28 23:48:43 -0300

    Criação dos repositories

---
+++ src/main/java/com/example/taskmanager/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.example.taskmanager.repository;
+
+import com.example.taskmanager.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByNomeIgnoreCase(String nome);
+}
