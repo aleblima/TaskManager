ad22cffc5062a0a4a0e2fd40900b1afae0caeceb
alejandro <alejandrolima1030@gmail.com>
2026-08-21 14:06:36 -0300

    finalizando authService

--- src/main/java/com/example/taskmanager/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/taskmanager/service/impl/AuthServiceImpl.java
@@ -11,12 +11,14 @@ import com.example.taskmanager.mapper.UsuarioMapper;
 import com.example.taskmanager.repository.UsuarioRepository;
 import com.example.taskmanager.security.JwtTokenProvider;
 import com.example.taskmanager.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
     private final UsuarioRepository usuarioRepository;
@@ -24,28 +26,17 @@ public class AuthServiceImpl implements AuthService {
     private final JwtTokenProvider jwtTokenProvider;
     private final UsuarioMapper usuarioMapper;
 
-    public AuthServiceImpl(
-            UsuarioRepository usuarioRepository,
-            PasswordEncoder passwordEncoder,
-            JwtTokenProvider jwtTokenProvider,
-            UsuarioMapper usuarioMapper) {
-        this.usuarioRepository = usuarioRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.usuarioMapper = usuarioMapper;
-    }
-
     @Override
     public UsuarioResponseDTO registrar(RegistroRequestDTO registroRequestDTO) {
          usuarioRepository.findByUsername(registroRequestDTO.username())
                  .ifPresent(usuario -> {
-                             throw new RegraDeNegocioException(String.format("%s está em uso ", usuario.getUsername()));
+                             throw new RegraDeNegocioException(String.format("%s está em uso", usuario.getUsername()));
                          });
         Usuario usuario = usuarioMapper.toEntity(registroRequestDTO);
         usuario.setSenha(passwordEncoder.encode(registroRequestDTO.senha()));
 
         Usuario usuarioSalvo = usuarioRepository.save(usuario);
-        return usuarioMapper.toDTO(usuarioSalvo);
+        return  usuarioMapper.toDTO(usuarioSalvo);
     }
 
     @Override
